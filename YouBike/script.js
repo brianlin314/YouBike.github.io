89db12693d55aec70abdd36241fe2a2068892e1b
Brian_Lin <74440668+brianlin314@users.noreply.github.com>
2023-04-12 23:00:07 +0800

    Update script.js

--- YouBike/script.js
+++ YouBike/script.js
@@ -5,14 +5,11 @@ let storeCluster;
 var x = 1;
 var dis = 0;
 var myLocation;
+
 const getCityDatas = getXML(
   "https://raw.githubusercontent.com/Feitoengineer19/mask-map/master/CityCountyData.json"
 );
 const getTaipeiDatas = getXML(  "https://tcgbusfs.blob.core.windows.net/dotapp/youbike/v2/youbike_immediate.json"
-);
-const getNewTaipeiDatas = getXML(  "https://cors-anywhere.herokuapp.com/https://data.ntpc.gov.tw/api/datasets/71CD1490-A2DF-4198-BEF1-318479775E8A/json/preview"
-);
-const gethsinpeiDatas = getXML(  "https://cors-anywhere.herokuapp.com/https://odws.hccg.gov.tw/001/Upload/25/opendataback/9059/59/f8bec8be-eb77-4c37-93e6-9245cd9961bc.json"
 );
 // 建立 Leaflet 地圖，並設定中心經緯度座標（台北市），縮放程度為 10。
 var map = L.map("map", {
@@ -37,18 +34,27 @@ L.control
 // 自訂座標 Icon。
 var greenIcon = new L.Icon({
   iconUrl:
-    "https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png",
+    "https://www.youbike.com.tw/region/assets/images/1.0-map-green.svg",
   shadowUrl:    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
-  iconSize: [25, 41],
+  iconSize: [40, 56],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
+});
+var orangeIcon = new L.Icon({
+  iconUrl:
+    "https://www.youbike.com.tw/region/assets/images/1.0-map-orange.svg",
+  shadowUrl:  "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
+  iconSize: [40, 56],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
   shadowSize: [41, 41]
 });
 var redIcon = new L.Icon({
   iconUrl:
-    "https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png",
+    "https://www.youbike.com.tw/region/assets/images/1.0-map-red.svg",
   shadowUrl:  "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
-  iconSize: [25, 41],
+  iconSize: [40, 56],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
   shadowSize: [41, 41]
@@ -72,11 +78,6 @@ L.marker(myLocation).addTo(map)
 
 var marks = new L.MarkerClusterGroup().addTo(map);
 var taipeimarkers = new L.MarkerClusterGroup().addTo(map);
-var newtaipeimarkers = new L.MarkerClusterGroup().addTo(map);
-var kaomarkers = new L.MarkerClusterGroup().addTo(map);
-var taomarkers = new L.MarkerClusterGroup().addTo(map);
-var taichungmarkers = new L.MarkerClusterGroup().addTo(map);
-var tainanmarkers = new L.MarkerClusterGroup().addTo(map);
 
 //台北市ubike位置標記
 var xhr = new XMLHttpRequest();
@@ -86,100 +87,18 @@ xhr.onload = function(){
  var data = JSON.parse(xhr.responseText)
  for(let i =0;data.length>i;i++){
   var danger;
-  if(data[i].sbi < 3){
+  if(data[i].sbi <= 3){
    danger = redIcon;
+  }
+  else if(data[i].sbi <= 10 && data[i].sbi > 3){
+    danger = orangeIcon
   }else{
    danger = greenIcon;
   } 
-  taipeimarkers.addLayer(L.marker([data[i].lat,data[i].lng], {icon: danger}).bindPopup("<strong>"+data[i].sna+"<strong/>"+"<h2>可還空位數:"+data[i].bemp+"<h2/>"+"<h2>可借車輛數:"+data[i].sbi+"<h2/>"));
+  taipeimarkers.addLayer(L.marker([data[i].lat,data[i].lng], {icon: danger}).bindPopup("<h3>"+data[i].sna+"<h3/>"+"<h3>可還空位數:"+data[i].bemp+"<h3/>"+"<h3>可借車輛數:"+data[i].sbi+"<h3/>"+"<h5>站點更新時間:<h5/>"+"<h5>"+data[i].srcUpdateTime+"<h5/>"));
  }
  map.addLayer(taipeimarkers);
 }
-//新北市ubike位置標記
-var newtaipeixhr = new XMLHttpRequest();
-newtaipeixhr.open("get","https://raw.githubusercontent.com/brianlin314/ubikejson/main/newtaipei.json");
-newtaipeixhr.send();
-newtaipeixhr.onload = function(){
- var data = JSON.parse(newtaipeixhr.responseText)
- for(let i =0;data.length>i;i++){
-  var danger;
-  if(data[i].sbi < 3){
-   danger = redIcon;
-  }else{
-   danger = greenIcon;
-  } 
-  newtaipeimarkers.addLayer(L.marker([data[i].lat,data[i].lng], {icon: danger}).bindPopup("<strong>"+data[i].sna+"<strong/>"+"<h2>可還空位數:"+data[i].bemp+"<h2/>"+"<h2>可借車輛數:"+data[i].sbi+"<h2/>"));
- }
- map.addLayer(newtaipeimarkers);
-}
-//高雄市ubike位置標記
-var kaoxhr = new XMLHttpRequest();
-kaoxhr.open("get","https://raw.githubusercontent.com/brianlin314/ubikejson/main/kao.json");
-kaoxhr.send();
-kaoxhr.onload = function(){
- var data = JSON.parse(kaoxhr.responseText).data.retVal
- for(let i =0;data.length>i;i++){
-  var danger;
-  if(data[i].sbi < 3){
-   danger = redIcon;
-  }else{
-   danger = greenIcon;
-  } 
-  kaomarkers.addLayer(L.marker([data[i].lat,data[i].lng], {icon: danger}).bindPopup("<strong>"+data[i].sna+"<strong/>"+"<h2>可還空位數:"+data[i].bemp+"<h2/>"+"<h2>可借車輛數:"+data[i].sbi+"<h2/>"));
- }
- map.addLayer(kaomarkers);
-}
-//桃園市ubike位置標記
-var taoxhr = new XMLHttpRequest();
-taoxhr.open("get","https://raw.githubusercontent.com/brianlin314/ubikejson/main/tao.json");
-taoxhr.send();
-taoxhr.onload = function(){
- var data = JSON.parse(taoxhr.responseText).records
- for(let i =0;data.length>i;i++){
-  var danger;
-  if(data[i].sbi < 3){
-   danger = redIcon;
-  }else{
-   danger = greenIcon;
-  } 
-  taomarkers.addLayer(L.marker([data[i].lat,data[i].lng], {icon: danger}).bindPopup("<strong>"+data[i].sna+"<strong/>"+"<h2>可還空位數:"+data[i].bemp+"<h2/>"+"<h2>可借車輛數:"+data[i].sbi+"<h2/>"));
- }
- map.addLayer(taomarkers);
-}
-//台中市ubike位置標記
-var taichungxhr = new XMLHttpRequest();
-taichungxhr.open("get","https://raw.githubusercontent.com/brianlin314/ubikejson/main/taichung.json");
-taichungxhr.send();
-taichungxhr.onload = function(){
- var data = JSON.parse(taichungxhr.responseText).retVal
- for(let i =0;data.length>i;i++){
-  var danger;
-  if(data[i].sbi < 3){
-   danger = redIcon;
-  }else{
-   danger = greenIcon;
-  } 
-  taichungmarkers.addLayer(L.marker([data[i].lat,data[i].lng], {icon:greenIcon }).bindPopup("<strong>"+data[i].sna+"<strong/>"+"<h2>可還空位數:"+data[i].bemp+"<h2/>"+"<h2>可借車輛數:"+data[i].sbi+"<h2/>"));
- }
- map.addLayer(taichungmarkers);
-}
-//台南市Tbike位置標記
-var tainanxhr = new XMLHttpRequest();
-tainanxhr.open("get","https://raw.githubusercontent.com/brianlin314/ubikejson/main/tainan.json");
-tainanxhr.send();
-tainanxhr.onload = function(){
- var data = JSON.parse(tainanxhr.responseText)
- for(let i =0;data.length>i;i++){
-  var danger;
-  if(data[i].sbi < 3){
-   danger = redIcon;
-  }else{
-   danger = greenIcon;
-  }  tainanmarkers.addLayer(L.marker([data[i].Latitude,data[i].Longitude], {icon: danger}).bindPopup("<strong>"+data[i].StationName+"<strong/>"+"<h2>可還空位數:"+data[i].AvaliableSpaceCount+"<h2/>"+"<h2>可借車輛數:"+data[i].AvaliableBikeCount+"<h2/>"));
- }
- map.addLayer(tainanmarkers);
-}
-
 // 串接 API，（非同步）獲取遠端 API 或本地 JSON 資料（封裝成一個 function）。
 function getXML(path) {
   // 利用 Promise 確保獲取資料完成。
@@ -231,7 +150,7 @@ Promise.all([getCityDatas, getTaipeiDatas]).then(resultDatas => {
         [str, storeCount] = createHTML(store, str, storeCount);
       }
     })
-    document.getElementById('store-total').innerHTML = `總共找到</u> ${storeCount} 家相符的藥局。`;
+    document.getElementById('store-total').innerHTML = `總共找到</u> ${storeCount} 家相符的站點。`;
     document.getElementById('store-list').innerHTML = str;
   })
 
@@ -265,7 +184,7 @@ function createAreaOption(cityData, citySelected) {
   areaSelect.innerHTML = areaOptionHTML;
 }
 
-// 產生藥局資訊的 HTML 樣板。
+// 產生站點資訊的 HTML 樣板。
 function createHTML(store, html, count, loved = false) {
   let [bgAdultColor, bgChildColor] = ['#bfffbf', '#bfffbf']; // 綠
   if(store.sbi <= 10) bgAdultColor = '#ffa470'; // 橙
@@ -276,7 +195,6 @@ function createHTML(store, html, count, loved = false) {
     <div class="store-info p-3" data-lat="${store.lat}" data-lng="${store.lng}">
       <div class="d-flex justify-content-between">
         <h5 class="font-weight-bold mb-2">${store.sna}</h5>
-      </div>
       <p class="mb-1"><i class="fas fa-map-marker-alt"></i>  <a href="https://www.google.com.tw/maps/place/${store.ar}" target="_blank">${store.ar}</a></p>
       <div class="masks-info">
         <div class="mask-item" style="background-color: ${bgAdultColor}">可借車輛數量 <span>${store.sbi}</span> 個</div>
@@ -328,19 +246,15 @@ function removeByValue(array, value) {
   })
 }
 
-
-
-
 function onMapClick(e) {
-  // 標記點擊區域
-  marks.clearLayers();   dis=Math.round(myLocation.distanceTo(e.latlng)/1000*1000)/1000;
-  var mark = L.marker(e.latlng)
-    .addTo(map)
-    .bindPopup("你與該位置的距離為 " + dis +"KM")
-    .openPopup();
-  var latlngs = [myLocation, e.latlng];
-  var polyline = L.polyline(latlngs, { color: "red" }).addTo(map);
-  marks.addLayer(mark);
-  marks.addLayer(polyline);
+    marks.clearLayers();   dis=Math.round(myLocation.distanceTo(e.latlng)/1000*1000)/1000;
+    var mark = L.marker(e.latlng)
+      .addTo(map)
+      .bindPopup("你與該位置的距離為 " + dis +"KM")
+      .openPopup();
+    var latlngs = [myLocation, e.latlng];
+    var polyline = L.polyline(latlngs, { color: "red" }).addTo(map);
+    marks.addLayer(mark);
+    marks.addLayer(polyline);
 }
 map.on("click", onMapClick);
